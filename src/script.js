c027338a9265b41ea207167b945276d1e488659c
octavia <octavia.ddo@gmail.com>
2021-10-21 15:51:59 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  //console.log(response.data);
+  console.log(response.data);
   //console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -32,6 +32,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp); //temperature will receive the value of response.data.main.temp
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -41,6 +42,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "0aad4c0893e446f9a51692da9cd797e8";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = Paris;
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
