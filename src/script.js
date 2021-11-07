a5200b749a2c93967ee8382db8926a3caf64fc24
octavia <octavia.ddo@gmail.com>
2021-11-07 18:04:42 +0000

    moved html inside script.js and creating the loop

--- src/script.js
+++ src/script.js
@@ -23,6 +23,38 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row>`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+          <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            
+            <img 
+            src="http://openweathermap.org/img/wn/50d@2x.png" 
+            alt="" 
+            width="42"/>
+            <div class="weather-forecast-temperatures"></div>
+              <span class="weather-forecast-temperatures-max">18</span>
+              <span class="weather-forecast-temperatures-min">12</span>
+            </div>
+          </div>
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
+//forecastElement.innerHTML =
+
 function displayTemperature(response) {
   console.log(response.data);
   //console.log(response.data.main.temp);
@@ -71,9 +103,10 @@ function displayFahrenheitTemperature(event) {
 }
 
 search("New York");
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
